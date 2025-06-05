f0c167076f16ec46d8d024d9746a501ee67341c2
Rejean-McCormick <boatbuiler610@gmail.com>
2025-06-05 11:46:40 -0400

    feat(ekoh): initial models & API

---
+++ konnaxion/konnaxion/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from konnaxion.users.models import User
+from konnaxion.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
